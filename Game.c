03ae206c63ff881122182105c70b3b0e8d7067f9
mosquito <1653828989@qq.com>
2019-10-03 13:45:49 +0800

    Update Game.c

--- Game.c
+++ Game.c
@@ -442,13 +442,13 @@ void Print()
 {
    //GotoXY(10,12);
    printf("\tWelcome to the mini Snake game.(press any key to continue)\n");
-  getch();
-   system("cls");
+  getch();              //下一步
+   system("cls");        //清屏
    printf("\tGame instructions:\n");
    printf("\n-> Use arrow keys to move the snake.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
    printf("\n\nPress any key to play game...");
-   if(getch()==27)
-   exit(0);
+    if(getch()==27)                                                                    //////////////存疑
+    exit(0);           //正常退出
 }
 void record(){
    char plname[20],nplname[20],cha,c;
